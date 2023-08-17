87690c482bc40cbcf39ef0610ebf8471a4bd1b8a
udbhav tripathi <udbhav.tripathi@unnatiagri.com>
2023-08-17 12:24:18 +0530

    fixed api settings

--- app.py
+++ app.py
@@ -55,22 +55,35 @@ def main():
     st.title("Course GPT")
     st.write("Create a personalized mini-course based on your particular application")
 
-    load_dotenv()
+    # load_dotenv()
+
+    # # Get user input for the API key
+    # api_key = st.text_input("Enter your API key:")
+
+    # # Update the .env file with the provided API key
+    # if api_key:
+    #     with open('.env', 'w') as env_file:
+    #         env_file.write(f'OPENAI_API_KEY={api_key}\n')
+    #     st.success("Success")
+
+    # # Reload environment variables after updating .env file
+    # load_dotenv()
+
+    # # Access the API key using os.getenv("API_KEY")
+    # api_key_from_env = os.getenv("OPENAI_API_KEY")
+
+
+
 
     # Get user input for the API key
     api_key = st.text_input("Enter your API key:")
 
-    # Update the .env file with the provided API key
     if api_key:
-        with open('.env', 'w') as env_file:
-            env_file.write(f'OPENAI_API_KEY={api_key}\n')
-        st.success("Success")
+        os.environ["OPENAI_API_KEY"] = api_key
+        st.success("API key set successfully")
 
-    # Reload environment variables after updating .env file
-    load_dotenv()
 
-    # Access the API key using os.getenv("API_KEY")
-    api_key_from_env = os.getenv("OPENAI_API_KEY")
+
 
      #1
     topic_name = st.text_input("Enter the topic name:")
